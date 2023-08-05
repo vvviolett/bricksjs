5f307eac8c2da067a328fafe0051da680bea5d4d
Chason <c18379908174@163.com>
2023-08-05 20:00:39 +0800

    feat: test PR

--- specs/isNumber.spec.ts
+++ specs/isNumber.spec.ts
@@ -1,5 +1,5 @@
 import { isNumber } from '@src/isNumber';
 
 test('cut 1 + 2 to equal 3', () => {
-  expect(isNumber(1)).toBe(true);
+  expect(isNumber(1)).toBe(false);
 });
--- specs/isString.spec.ts
+++ specs/isString.spec.ts
@@ -1,5 +1,5 @@
 import { isString } from '@src/isString';
 
 test('cut 1 + 2 to equal 3', () => {
-  expect(isString('1')).toBe(true);
+  expect(isString('1')).toBe(false);
 });
